8fab8ee7a8053f7901d2bc4dd8103ac796076b92
Shivdatt Santosh Jangam <115657506+shivGam@users.noreply.github.com>
2023-03-02 00:16:41 +0530

    Add files via upload

--- pygui.py
+++ pygui.py
@@ -5,7 +5,7 @@ app=Tk()
 app.title('First Fit Management scheme')
 app.geometry('650x600')
 table=ttk.Treeview(app)
-#app.configure(bg="#141414")
+app.configure(bg="#141414")
 
 def submitValue():
 
@@ -24,7 +24,7 @@ def submitValue():
 def treeview(blockSizedDE):
     
     table['columns']=('Process Allocated','Blocks Declared')
-    table.grid(row=5,column=1)
+    table.place(x=200,y=300)
 
     table.column("#0",width=0,stretch=NO)
     table.column("Process Allocated",anchor=CENTER,width=120)
@@ -38,39 +38,41 @@ def treeview(blockSizedDE):
     for record in blockSizedDE:
         table.insert(parent='',index='end',text='',values=(blockSizedDE[count]))
         count +=1
-    #
 
 def allot():
-    print('alot')
+    print('alott')
 
 def clear():
     blockEntry.delete(0,END)
     numBlockEntry.delete(0,END)
     processEntry.delete(0,END)
+    for record in table.get_children():
+        table.delete(record)
+    
 
 numBlock=StringVar()
-numBlockLabel=Label(app,text='Number of blocks: ',font=('bold',12),pady=20,padx=10)
+numBlockLabel=Label(app,text='Number of blocks: ',bg='#141414',fg='#FFFFFF',font=('bold',12),pady=20,padx=10)
 numBlockLabel.grid(row=0,column=0,sticky=W)
 numBlockEntry=Entry(app,width=60,textvariable=numBlock)
 numBlockEntry.grid(row=0,column=1)
 
 blockSize=StringVar()
-blockLabel=Label(app,text='Declare Blocks: ',font=('bold',12),pady=20,padx=10)
+blockLabel=Label(app,text='Declare Blocks: ',bg='#141414',fg='#FFFFFF',font=('bold',12),pady=20,padx=10)
 blockLabel.grid(row=1,column=0,sticky=W)
 blockEntry=Entry(app,width=60,textvariable=blockSize)
 blockEntry.grid(row=1,column=1)
 
 processSize=StringVar()
-processLabel=Label(app,text='Process size: ',font=('bold',12),pady=20,padx=10)
+processLabel=Label(app,text='Process size: ',bg='#141414',fg='#FFFFFF',font=('bold',12),pady=20,padx=10)
 processLabel.grid(row=2,column=0,sticky=W)
 processEntry=Entry(app,width=60,textvariable=processSize)
 processEntry.grid(row=2,column=1)
 
-submitButton=Button(app,text="Submit",width=12,command=submitValue)
+submitButton=Button(app,text="Submit",width=14,height=2,command=submitValue)
 submitButton.grid(row=4,column=0)
-clearButton=Button(app,text="Clear",width=12,command=clear)
+clearButton=Button(app,text="Clear",width=14,height=2,command=clear)
 clearButton.grid(row=4,column=1)
-goButton=Button(app,text='GO',width=12,command=allot)
+goButton=Button(app,text='GO',width=14,height=2,command=allot)
 goButton.grid(row=4,column=2)
 
 
